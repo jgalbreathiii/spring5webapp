c08cb12d0e28f77c8c34693998798927dd1edfea
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-11 17:10:53 -0400

    Completed all requirements for Assignment 2

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,22 +1,41 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     private String title;
     private String isbn;
-    private Set<Author> authors;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
 
     public Book()
     {
     }
 
-    public Book(String title, String isbn, Set<Author> authors)
+    public Book(String title, String isbn)
     {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
     }
 
     public String getTitle()
@@ -47,5 +66,39 @@ public class Book
     public void setAuthors(Set<Author> authors)
     {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (int) (id ^ (id >>> 32));
     }
 }
