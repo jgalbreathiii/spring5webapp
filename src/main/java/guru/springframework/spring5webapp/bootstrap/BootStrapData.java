7e7d8fbaf1f084aa28bec881e5087b0903c6f65b
jgalbreathiii <jgalbreathiii@gmail.com>
2020-05-14 13:13:03 -0400

    Added content from Lecture 18

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -26,28 +26,37 @@ public class BootStrapData implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception
     {
+        Publisher joeyPublishing = new Publisher("Joey's Publishing", "123 Main St", "Ypsilanti", "Michigan", "48197");
+
+        publisherRepository.save(joeyPublishing);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(joeyPublishing);
+        joeyPublishing.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(joeyPublishing);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(joeyPublishing);
+        joeyPublishing.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher joeyPublishing = new Publisher("Joey's Publishing", "123 Main St", "Ypsilanti", "Michigan", "48197");
-
         publisherRepository.save(joeyPublishing);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Publisher Number of Books: " + joeyPublishing.getBooks().size());
     }
 }
